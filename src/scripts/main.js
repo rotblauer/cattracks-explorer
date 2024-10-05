104009554bb2fd4315d269512dd355ee4d90f1d5
ia <isaac.ardis@gmail.com>
2024-10-05 09:05:41 -0600

    refactor
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -85,84 +85,87 @@ import * as turf from '@turf/turf';
         return color;
     }
 
+    const paintCircle = {
+        // https://maplibre.org/maplibre-style-spec/layers/#paint-circle-circle-color
+        // https://docs.mapbox.com/mapbox-gl-js/example/data-driven-circle-colors/
+        // 'circle-color': "#000000",
+        'circle-color': [
+            'case',
+            ['boolean', ['feature-state', 'hover'], false], '#FFFF00',
+            ['==', ['get', 'IsTrip'], true], '#0000FF',
+            ['==', ['get', 'IsTrip'], false], '#FF0000',
+            '#880000',
+        ],
+        'circle-opacity': [
+            'case',
+            ['<', ['get', 'Duration'], 60 * 10], 0.02,
+            ['<', ['get', 'Duration'], 60 * 60], 0.04,
+            ['<', ['get', 'Duration'], 60 * 60 * 8], 0.08,
+            ['<', ['get', 'Duration'], 60 * 60 * 24], 0.16,
+            ['<', ['get', 'Duration'], 60 * 60 * 72], 0.32,
+            0.25,
+        ],
+        'circle-radius': [
+            'case',
+            ['<', ['get', 'Duration'], 60 * 10], 2,
+            ['<', ['get', 'Duration'], 60 * 60], 4,
+            ['<', ['get', 'Duration'], 60 * 60 * 8], 6,
+            ['<', ['get', 'Duration'], 60 * 60 * 24], 8,
+            ['<', ['get', 'Duration'], 60 * 60 * 72], 10,
+            4,
+        ],
+        // 'circle-radius': [
+        //     "interpolate",
+        //     ["exponential", 0.9999],
+        //     // ["get", "Count"],
+        //     ["get", "Duration"],
+        //     1, 2,
+        //     60 * 60 * 8, 20,
+        // ],
+        // 'circle-radius': [
+        //     'case',
+        //     ['==', ['get', 'IsTrip'], true], 2,
+        //     ['==', ['get', 'IsTrip'], false], 6,
+        //     2,
+        // ],
+        // 'line-color': getNextColor,
+        // 'line-width': 2,
+    };
+
+    const paintLine = {
+        'line-color': [
+            'match',
+            ['get', 'Activity'],
+            'Stationary', '#f32d2d',
+            'Walking', '#e78719',
+            'Running', '#028532',
+            'Bike', '#3112f6',
+            'Automotive', '#d670fa',
+            'Unknown', '#444444',
+            '#888888',
+        ],
+        // 'line-width': 4,
+        'line-width': [
+            'case',
+            ['boolean', ['feature-state', 'hover'], false], 8,  // HOVER DOES NOT WORK
+            4,
+        ],
+        'line-opacity': [
+            'case',
+            ['boolean', ['feature-state', 'hover'], false], 1,
+            0.33,
+        ]
+
+        // 'line-gap-width': 2,
+        // 'line-cap': 'round',
+    };
+
     function paintFor(layerType) {
         switch (layerType) {
             case 'circle':
-                return {
-                    // https://maplibre.org/maplibre-style-spec/layers/#paint-circle-circle-color
-                    // https://docs.mapbox.com/mapbox-gl-js/example/data-driven-circle-colors/
-                    // 'circle-color': "#000000",
-                    'circle-color': [
-                        'case',
-                        ['boolean', ['feature-state', 'hover'], false], 'yellow',
-                        ['==', ['get', 'IsTrip'], true], '#0000FF',
-                        ['==', ['get', 'IsTrip'], false], '#FF0000',
-                        '#880000',
-                    ],
-                    'circle-opacity': [
-                        'case',
-                        ['<', ['get', 'Duration'], 60 * 10], 0.02,
-                        ['<', ['get', 'Duration'], 60 * 60], 0.04,
-                        ['<', ['get', 'Duration'], 60 * 60 * 8], 0.08,
-                        ['<', ['get', 'Duration'], 60 * 60 * 24], 0.16,
-                        ['<', ['get', 'Duration'], 60 * 60 * 72], 0.32,
-                        0.25,
-                    ],
-                    // 'circle-radius': 4,
-                    'circle-radius': [
-                        'case',
-                        ['<', ['get', 'Duration'], 60 * 10], 2,
-                        ['<', ['get', 'Duration'], 60 * 60], 4,
-                        ['<', ['get', 'Duration'], 60 * 60 * 8], 6,
-                        ['<', ['get', 'Duration'], 60 * 60 * 24], 8,
-                        ['<', ['get', 'Duration'], 60 * 60 * 72], 10,
-                        4,
-                    ],
-                    // 'circle-radius': [
-                    //     "interpolate",
-                    //     ["exponential", 0.9999],
-                    //     // ["get", "Count"],
-                    //     ["get", "Duration"],
-                    //     1, 2,
-                    //     60 * 60 * 8, 20,
-                    // ],
-                    // 'circle-radius': [
-                    //     'case',
-                    //     ['==', ['get', 'IsTrip'], true], 2,
-                    //     ['==', ['get', 'IsTrip'], false], 6,
-                    //     2,
-                    // ],
-                    // 'line-color': getNextColor,
-                    // 'line-width': 2,
-                };
+                return paintCircle;
             case 'line':
-                return {
-                    'line-color': [
-                        'match',
-                        ['get', 'Activity'],
-                        'Stationary', '#f32d2d',
-                        'Walking', '#e78719',
-                        'Running', '#028532',
-                        'Bike', '#3112f6',
-                        'Automotive', '#d670fa',
-                        'Unknown', '#444444',
-                        '#888888',
-                    ],
-                    // 'line-width': 4,
-                    'line-width': [
-                        'case',
-                        ['boolean', ['feature-state', 'hover'], false], 8,  // HOVER DOES NOT WORK
-                        4,
-                    ],
-                    'line-opacity': [
-                        'case',
-                        ['boolean', ['feature-state', 'hover'], false], 1,
-                        0.33,
-                    ]
-
-                    // 'line-gap-width': 2,
-                    // 'line-cap': 'round',
-                };
+                return paintLine;
         }
         return {};
     }
@@ -282,8 +285,8 @@ import * as turf from '@turf/turf';
     $("#map").append($featureDebugWindow);
 
     // https://maplibre.org/maplibre-gl-js/docs/examples/popup-on-hover/
-    function addInspectPopup(layerId) {
-        map.on('mouseenter', layerId, (e) => {
+    function addInspectPopup(sourceLayerID) {
+        map.on('mouseenter', sourceLayerID, (e) => {
 
             // // https://maplibre.org/maplibre-gl-js/docs/examples/hover-styles/
             // if (e.features.length > 0) {
@@ -341,7 +344,7 @@ import * as turf from '@turf/turf';
             $featureDebugWindow.html(`<code>${description}</code>`);
         });
 
-        map.on('mouseleave', layerId, () => {
+        map.on('mouseleave', sourceLayerID, () => {
             map.getCanvas().style.cursor = '';
             setTimeout(() => {
                 // popup.remove(); // from map
@@ -380,21 +383,16 @@ import * as turf from '@turf/turf';
 
         let params = new URLSearchParams(window.location.search);
         console.debug("params", params);
+
+        // First we iterate the 'geojson=' targets, splitting on commas.
         let geojsonDataTargets = [];
         if (params.get("geojson"))  {
             geojsonDataTargets = params.get("geojson").split(",");
         }
-        let vectorTargets = [];
-        if (params.get("vector")) {
-            vectorTargets = params.get("vector").split(",");
+        if (geojsonDataTargets.length === 0) {
+            console.debug("WARN: No geojson data sources found in URL. Use like: localhost:8080/public/?geojson=http://localhost:3001/cattraks.json");
         }
-        if (vectorTargets.length === 0 && geojsonDataTargets.length === 0) {
-            console.error("No data sources found in URL. Noop.");
-            console.error("Use like: localhost:8080/public/?vector=http://localhost:3001/services/ia/naps/tiles/{z}/{x}/{y}.pbf,http://localhost:z/services/ia/laps/tiles/{z}/{x}/{y}.pbf")
-        }
-        // split targets by comma and iterate the items
         for (let target of geojsonDataTargets) {
-
             console.debug("geojson target", target);
 
             // await causes the data to be rendered in the order
@@ -414,10 +412,27 @@ import * as turf from '@turf/turf';
                 console.error(err);
             });
         }
+
+        // Then we iterate the 'vector=' targets, splitting on commas.
+        let vectorTargets = [];
+        if (params.get("vector")) {
+            vectorTargets = params.get("vector").split(",");
+        }
+        if (vectorTargets.length === 0) {
+            console.debug("WARN: No vector data sources found in URL. Use like: http://localhost:8080/public/?vector=http://localhost:3001/services/ia/naps/tiles/{z}/{x}/{y}.pbf,http://localhost:3001/services/ia/laps/tiles/{z}/{x}/{y}.pbf");
+        }
         for (let target of vectorTargets) {
-            console.debug("vector target", target);
+            console.debug("DEBUG: Vector target.", target);
+            // Get a source layer name from target URL, eg. 'naps' or 'laps', as in 'http://localhost:3001/services/rye/{naps,laps}/tiles/...'.
+            // Deriving this name from the target ASSUMES a conventional URI scheme from TIPPECANOE.
+            // It is index=5 in the URL path, splitting on '/'s.
+            // For example:
+            //   http://localhost:3001/services/ia/naps/tiles/{z}/{x}/{y}.pbf => 'naps'
+            //   http://localhost:3001/services/ia/valid/tiles/{z}/{x}/{y}.pbf => 'valid'
+            const sourceID = target.split("/")[4] + '-' + target.split("/")[5]; // => 'rye-naps'
+
             // Vector source.
-            map.addSource(target, {
+            map.addSource(sourceID, {
                 'type': 'vector',
                 'tiles': [target],
                 'minzoom': 1,
@@ -425,87 +440,78 @@ import * as turf from '@turf/turf';
                 // 'promoteId': 'Time',
             });
 
-            // Circle layer.
-            // Get source layer from target URL.
-            // This 'source layer' follows the conventional name scheme from tippecanoe.
-            // It is index=5 in the URL path, splitting on '/'s.
-            // http://localhost:3001/services/ia/naps/tiles/{z}/{x}/{y}.pbf => 'naps'
-            // http://localhost:3001/services/ia/valid/tiles/{z}/{x}/{y}.pbf => 'valid'
-            const sourceLayer = target.split("/")[5];
+            console.debug(`DEBUG: Add vector target source. '${target}' `, 'sourceLayerID=', sourceID);
+
+            let addLayerObject = {
+                id: `layer-${sourceID}`,
+                source: sourceID,
+                // 'source-layer' is the NAME of the layer in the source tiles.
+                // By my own convention, these are also the NAMEs of the SERVICE.
+                // Eg. 'naps', 'laps', 'valid'.
+                'source-layer': sourceID.split("-")[1],
+                // 'type': 'TODO',
+                // 'paint': paintFor('line'),
+                // 'filter': [
+                //     'all',
+                //     ['>', 'PointCount', 30],
+                //     ['<', 'Duration', 86000],
+                //     ['<', 'AverageAccuracy', 200],
+                // ]
+            };
 
             // Static points (naps, valid) should be represented as circles.
-            if (sourceLayer === 'naps') {
-                // This adds a 'circle' layer for laps.
-                map.addLayer({
-                    'id': `layer-circle-naps-${target}`,
-                    'source': target,
-                    'source-layer': sourceLayer,
-                    'type': 'circle',
-                    'paint': paintFor('circle'),
-                    filter: [
-                        'all',
-                        // [ 'has', 'point_count'],
-                        // [ '>', 'point_count', 30],
-                        // ["has", "Count"],
-                        [">", "Count", 100],
-                        // [">", "Duration", 60],
-                    ]
-                });
+            if (/naps/.test(target)) {
+                console.debug("DEBUG: Vector target is naps.");
 
-                // For vector sources, sourceLayer is required.
-                addHoverState(target, `layer-circle-naps-${target}`);
-                addInspectPopup(`layer-circle-naps-${target}`);
-            } else if (sourceLayer === 'valid') {
-                // This adds a 'circle' layer for laps.
-                let _paint = paintFor('circle');
-                _paint["circle-color"] = "#888888";
-                _paint["circle-opacity"] = 0.8;
-                _paint["circle-radius"] = 2;
-                map.addLayer({
-                    'id': `layer-circle-valid-${target}`,
-                    'source': target,
-                    'source-layer': sourceLayer,
-                    'type': 'circle',
-                    'paint': _paint,
-                    filter: [
-                        'all',
-                        // [ 'has', 'point_count'],
-                        // [ '>', 'point_count', 30],
-                        // ["has", "Count"],
-                        // [">", "Count", 1],
-                        // [">", "Duration", 60],
-                    ]
-                });
-                // For vector sources, sourceLayer is required.
-                addHoverState(target, `layer-circle-valid-${target}`);
-                addInspectPopup(`layer-circle-valid-${target}`);
-            } else {
-                // Line layer.
+                addLayerObject.type = 'circle';
+                addLayerObject.paint = paintFor('circle');
+                addLayerObject.filter = [
+                    'all',
+                    [">", "Count", 100],
+                ];
+
+            } else if (/valid/.test(target)) {
+
+                addLayerObject.type = 'circle';
+                addLayerObject.paint = paintFor('circle');
+                addLayerObject.paint["circle-color"] = "#888888";
+                addLayerObject.paint["circle-opacity"] = 0.8;
+                addLayerObject.paint["circle-radius"] = 2;
+                addLayerObject.filter = [
+                    'all',
+                    // [ 'has', 'point_count'],
+                    // [ '>', 'point_count', 30],
+                    // ["has", "Count"],
+                    // [">", "Count", 1],
+                    // [">", "Duration", 60],
+                ];
+
+            } else if (/laps/.test(target)) {
+
                 // Note: that it might be useful to also represent the comprising points
                 // on the line as individual points in order to visualize the outcome
                 // of any simplification logic.
                 // TODO: add circle layer too?
 
-                map.addLayer({
-                    'id': `layer-line-${target}`,
-                    'source': target,
-                    'source-layer': 'laps',
-                    'type': 'line',
-                    'paint': paintFor('line'),
-                    'filter': [
-                        'all',
-                        ['>', 'PointCount', 30],
-                        ['<', 'Duration', 86000],
-                        // ['>=', 'Speed', 1],
-                        // ['>', 'Duration', 60],
-                        // ['!=', 'Activity', 'Stationary'],
-                        // ['!=', 'Activity', 'Unknown'],
-                        ['<', 'AverageAccuracy', 200],
-                    ]
-                });
-                addHoverState(target, `layer-line-${target}`);
-                addInspectPopup(`layer-line-${target}`);
+                addLayerObject.type = 'line';
+                addLayerObject.paint = paintFor('line');
+                addLayerObject.filter = [
+                    'all',
+                    ['>', 'PointCount', 30],
+                    ['<', 'Duration', 86000],
+                    // ['>=', 'Speed', 1],
+                    // ['>', 'Duration', 60],
+                    // ['!=', 'Activity', 'Stationary'],
+                    // ['!=', 'Activity', 'Unknown'],
+                    ['<', 'AverageAccuracy', 35],
+                ];
             }
+
+            map.addLayer(addLayerObject);
+
+            // For vector sources, sourceLayer is required.
+            addHoverState(sourceID, `layer-${sourceID}`);
+            addInspectPopup(`layer-${sourceID}`);
         }
 
 
